1b8602f3c225a8ee33f02e20721649c9be3795f9
Eve Weinberg <eve@neveroddoreven.tv>
2016-02-05 19:55:55 -0500

    fridaynight

--- WEEK2/main/sketch.js
+++ WEEK2/main/sketch.js
@@ -20,7 +20,7 @@ var faceWidth = 190,
 
 for (var g = 0; g < 20; g++) {
     for (var h = 0; h < 20; h++) {
-        stars[g] = new star(Rune.random(70, 130) * g, Rune.random(70,130) * h, 20);
+        stars[g] = new star(Rune.random(70, 130) * g, Rune.random(70,130) * h, 10);
     }
 }
 
@@ -51,14 +51,14 @@ for (var i = 0; i < 18; i++) {
 ///MAIN DUDE BEGIN/////////
 
 //backpack
-var backpackwidth = faceWidth * 1.4;
+var backpackwidth = faceWidth * 1.2;
 var backpackHeight = faceHeight * 3
     r.rect(r.width / 2 - backpackwidth/2, r.height / 2 - (faceHeight+60), backpackwidth, backpackHeight, eyes).strokeCap("round").strokeJoin("round").fill(black).stroke(white).strokeWidth(thickestStroke);
 
     r.ellipse(r.width / 2, r.height / 2, faceWidth * 1.2, faceWidth * 1.2).fill(black).stroke(white).strokeWidth(thickestStroke);
 
 
-eyes(faceCenterX + 40, 400);
+// eyes(faceCenterX + 40, 400);
 eyes(faceCenterX - 40, 400);
 
 
@@ -68,10 +68,10 @@ r.draw();
 
 function star(moveX, moveY, length) {
     var stargroup = r.group(moveX, moveY).rotate(Rune.random(90),moveX,moveY)
-    r.rect(0, 0, 1, length, stargroup).fill(white).stroke(false).strokeCap("round");
-    r.rect(0, 0, 1, length, stargroup).fill(white).stroke(false).rotate(270).strokeCap("round");
-    r.rect(0, 0, 1, length, stargroup).fill(white).stroke(false).rotate(90).strokeCap("round");
-    r.rect(0, 0, 1, length, stargroup).fill(white).stroke(false).rotate(180).strokeCap("round");
+    r.rect(0, 0, .5, length, stargroup).fill(white).stroke(false).strokeCap("round");
+    r.rect(0, 0, .5, length, stargroup).fill(white).stroke(false).rotate(270).strokeCap("round");
+    r.rect(0, 0, .5, length, stargroup).fill(white).stroke(false).rotate(90).strokeCap("round");
+    r.rect(0, 0, .5, length, stargroup).fill(white).stroke(false).rotate(180).strokeCap("round");
 
 }
 
@@ -80,18 +80,21 @@ function eyes(moveX, moveY) {
     var radWide = 60;
     var radMid = 35;
     
+    r.rect(r.width / 2 - (faceWidth*.9 / 2), r.height / 2 - (faceHeight / 2), faceWidth*.9, faceHeight).strokeCap("round").strokeJoin("round").fill(white).stroke(black).strokeWidth(4);
 
 var eyes = r.group(0,0)
-    r.rect(r.width / 2 - (faceWidth*.9 / 2), r.height / 2 - (faceHeight / 2), faceWidth*.9, faceHeight).strokeCap("round").strokeJoin("round").fill(false).stroke(white).strokeWidth(4);
     
     //ears
     var ear =r.rect(r.width / 2 - (faceWidth - radWide), r.height / 2 - (faceHeight - 40), 20,70).strokeCap("round").strokeJoin("round").fill(black).stroke(white).strokeWidth(4);
 
     r.rect(r.width / 2 + (faceWidth - radWide-15), r.height / 2 - (faceHeight - 40), 20, 70).strokeCap("round").strokeJoin("round").fill(black).stroke(white).strokeWidth(4);
 //eyes
-    r.ellipse(0, 0, radWide, radWide, eyes).fill(black).stroke(white).move(moveX, moveY);
-    r.ellipse(0, 0, radMid, radMid, eyes).fill(black).stroke(white).move(moveX, moveY);
-    r.ellipse(0, 0, 5, 5, eyes).fill(white).stroke(white).move(moveX, moveY);
+    r.ellipse(0, 0, radWide, radWide, eyes).fill(white).stroke(black).move(moveX, moveY).strokeWidth(4);
+    r.ellipse(0, 0, radMid, radMid, eyes).fill(white).stroke(black).move(moveX, moveY).strokeWidth(4);
+    r.ellipse(0, -10, 5, 5, eyes).fill(black).stroke(black).move(moveX, moveY-5);
+     r.ellipse(0, 0, radWide, radWide, eyes).fill(white).stroke(black).move(moveX+80, moveY).strokeWidth(4);
+    r.ellipse(0, 0, radMid, radMid, eyes).fill(white).stroke(black).move(moveX+80, moveY).strokeWidth(4);
+    r.ellipse(0, -10, 5, 5, eyes).fill(black).stroke(black).move(moveX+80, moveY-5);
 
 // eyes.scale(.5)
 

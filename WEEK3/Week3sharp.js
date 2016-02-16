56fe583d89e17728f7fb0543d7bc20603676b2b7
Eve Weinberg <eve@neveroddoreven.tv>
2016-02-15 19:58:30 -0500

    wet

--- WEEK3/Week3sharp.js
+++ WEEK3/Week3sharp.js
@@ -1,8 +1,8 @@
 //ignore this
 var r = new Rune({
     container: "#canvas",
-    width: 800,
-    height: 800,
+    width: 1200,
+    height: 1200,
     debug: true
 });
 
@@ -10,31 +10,51 @@ var r = new Rune({
 var black = 0;
 var white = 255;
 ///////////////////////
-var steps = 40;
-var radius = 50;
+var steps = 20;
+var numOfSparks = Rune.random(3,24);
+var radiusBase = numOfSparks*27;
 var startX = 0,
     startY = 0;
+var fillCol = white;
+var strokeCol = black;
+var dash1 = Rune.random(10, 50),
+    dash2 = Rune.random(20, 100);
 ///////////////////////
 
-
-var sharpGroup = r.group(0, 0);
+var shape = r.polygon(r.width / 2, 125 + (200));
+var spacing = 360 / steps;
+var sharpGroup = r.group(200, 200);
 var sharp = r.polygon(startX, startY, sharpGroup)
 
 // var shape = r.polygon(lineEndx, LineEndy, raindropGroup);
 
+  var test = new Rune.Path(0,0)
+for (var i = 0; i < numOfSparks; i++) {
+    var test = r.polygon(r.width/2,r.height/2)
+    if (i % 2 == 0) {
+        console.log("even")
+        fillCol = white;
+        strokeCol = black;
+    } else {
+        console.log("odd")
+        fillCol = black;
+        strokeCol = white;
+    }
+    for (var j = 0; j < steps*2; j++) {
+        if (j % 2 == 0) {
+            radius = radiusBase + (Rune.random(70, 190)) - (i * 50);
+            console.log(radius, i)
+        } else {
+            radius = radiusBase - (Rune.random(20, 40)) - (i * 50);
+        }
+        var x = Rune.random(20, 40) + Math.cos(Rune.radians(j * spacing)) * radius;
+        var y = Math.sin(Rune.radians(j * spacing)) * radius;
+        
+        test.lineTo(x, y)
+            // .strokeDash("10,90," + dash1 / 2 + "," + dash2 / 2)
+            .fill(fillCol).stroke(strokeCol).strokeWidth(1);
+    }
 
-for (var i = 0; i < (360 / steps); i++) {
-    var x = Math.cos(Rune.radians(j * spacing)) * i * radius;
-    var y = .5 * Math.sin(Rune.radians(j * spacing)) * i * radius;
-    sharp.lineTo(x, y)
-        .fill(255, 0, 0);
-}
-
-for (var j = 0; j < 360; j++) {
-    ringTime = true;
-    var x = Math.cos(Rune.radians(j * spacing)) * i * radius;
-    var y = .5 * Math.sin(Rune.radians(j * spacing)) * i * radius;
-    shape.lineTo(x, y).strokeDash("10,90," + dash1 / 2 + "," + dash2 / 2);
 }
 
 
--- WEEK3/Week3wet.js
+++ WEEK3/Week3wet.js
@@ -1,62 +1,150 @@
-//ignore this
-var r = new Rune({
-    container: "#canvas",
-    width: 800,
-    height: 800,
-    debug: true
-});
-
-////////////////////////
 var black = 0;
 var white = 255;
-///////////////////////
 var raindrops = [];
-var number = 0;
+// var number = 0;
 var movingX = 0,
     movingY = 0,
     deg = Rune.random(0, 50);
 var numberOfpts = 3;
 var pointDegree = 360 / numberOfpts; //how much to rotate every time
-
-// number++;
-// // movingX = Math.cos(i*Rune.radians(deg))*100;
-// // movingY = Math.cos(i*Rune.radians(deg))*100;
-// var myPoly = r.polygon(200, 50);
-// r.circle(0, 0, 50);
-// r.circle(movingX*100,movingY*100,50);
-
-// for (var i = 0; i < numberOfpts; i++) {
-//     movingX = Math.cos(Rune.radians(i * pointDegree)) * 100;
-//     movingY = Math.cos(Rune.radians(i * pointDegree)) * 100;
-//     r.circle(movingX, movingY, 30)
-//     myPoly.lineTo(movingX, movingY);
-// }
+var raindropCols = Rune.random(2, 6)
+var RndmRowNum = Rune.random(9, 9);
+var noise = new Rune.Noise();
+var noiseStep = 0;
+var rw = $(window).width();
+var bgCols = []
+var randomvalue ;
+var bgRandom ;
 
 
-// var it  =r.polygon(0, 0)
-//     .lineTo(0, 0)
-//     .lineTo(200, 200)
-//     .lineTo(-00, -900)
-//     .lineTo(0, 0)
-//     .fill(255, 0, 0);
+Everything();
 
-// var YellowTri = r.path(0, 100)
-//     .lineTo(400, 100)
-//     .curveTo(100, 400, 400, 100, 600, 200)
-//     .fill(255, 255, 0);
 
-// console.log(it.centroid()); 
-for (var i = 0; i < 30; i++) {
-    var moveLeft = Rune.random(-1200, 1200)
-    var moveDown = Rune.random(30, 300);
-    raindrops[i] = new Raindrop(r.width - moveLeft, 0 - moveDown, 100 - moveLeft, r.height - moveDown, Rune.random(1, 8), Rune.random(20, 100), Rune.random(20, 100), Rune.random(2, 5));
+/////////////////
+$('#refresh').click(function() {
+    // location.reload();
+    Everything();
+    $("svg").css({ "background-color": bgRandom });
+     // $("svg").css({ "background-color": "hsl(320, .5,.7)" });
+});
+////////////////
+$('#save').click(function(){
+// var canvas = document.getElementById("canvas");
+var canvas = $("#canvas");
+saveCanvas(canvas, 'myCanvas', 'jpg');
+    // var img   = canvas.toDataURL("image/png");
+    // document.write('<img src="'+img+'"/>');
+});
+
+function Everything(){
+////////////////////////
+var black = 0;
+var white = 255;
+var raindrops = [];
+// var number = 0;
+var movingX = 0,
+    movingY = 0,
+    deg = Rune.random(0, 50);
+var numberOfpts = 3;
+var pointDegree = 360 / numberOfpts; //how much to rotate every time
+var raindropCols = Rune.random(2, 6)
+var RndmRowNum = Rune.random(9, 9);
+
+var noise = new Rune.Noise();
+var noiseStep = 0;
+
+var rw = $(window).width();
+////////////////////////////////////////////////////////////////////
+
+
+
+var r = new Rune({
+    container: "#canvas",
+    width: rw,
+    height: 800,
+    debug: true
+});
+
+var bgCols = ["#1ab3c9", "#2acef8", "#0c707e", "#1fbfcc", "#133c69","#068d98","#1ac9c9"]
+
+randomvalue = Math.floor(Rune.random(0,bgCols.length));
+bgRandom = bgCols[randomvalue];
+
+var noisePath = r.path(0, 300)
+    .stroke(255)
+    .fill(false)
+    .strokeWidth(2);
+
+
+
+
+
+
+
+
+//call the raindrop function here, push to an array //////
+var startX = -500;
+var startY = -100;
+var lineLength = 700;
+var lineWidth = 500;
+var oddAdder;
+for (var i = 0; i < raindropCols; i++) {
+    for (var j = 0; j < RndmRowNum; j++) {
+        var moveLeft = Rune.random(300, 500);
+        var moveDown = Rune.random(40, 60);
+        if (i % 2 == 0) {
+            oddAdder = moveLeft / 3;
+        } else {
+            oddAdder = 0;
+        }
+        raindrops[i] = new Raindrop(startX + oddAdder + 500 + moveLeft * j, startY - moveDown * i, startX + oddAdder + 500 + moveLeft * j - lineWidth, startY - moveDown * i + lineLength, Rune.random(.2, 6), Rune.random(20, 100), Rune.random(20, 100), Rune.random(1, 5));
+
+
+    }
+    // r.ellipse(r.width/2,startY-200+lineLength,rw*2,300).fill(false).stroke(white);
 }
 
 
+/////////////main frame////////////
 
 
 
+for (var i = 0; i < 6; i++) {
+    var xStep = 15;
+
+
+    if (i > 3) {
+        var lineheight = startY + 700;
+
+    } else {
+        var lineheight = startY + 300;
+    }
+
+    noise.noiseSeed(Rune.random(100));
+
+    var noisePath = r.path(0, lineheight + (i * 30))
+        .stroke(255)
+        .fill(false)
+        .strokeWidth(2);
+
+    var noiseStep = 0;
+
+    for (var x = 0; x < r.width + 20; x += xStep) {
+        var y = noise.get(noiseStep) * 100;
+        noisePath.lineTo(x, y);
+
+        noiseStep += 0.05;
+    }
+}
+///////////////////////////////////
+
+
+
+
+
+//////////////////
 function Raindrop(lineStartx, lineSarty, lineEndx, LineEndy, lineStroke, dash1, dash2, randomRingAmt) {
+
     var raindropGroup = r.group(0, 0);
     var splashWidth = 60;
     var ringTime = false;
@@ -64,35 +152,40 @@ function Raindrop(lineStartx, lineSarty, lineEndx, LineEndy, lineStroke, dash1, 
     var radius = 40;
     var points = 40;
 
-    if (ringTime == true){
-        lineStroke=lineStroke-2;
-    } else{
+    if (ringTime == true) {
+        lineStroke = lineStroke - 2;
+    } else {
         lineStroke = lineStroke;
     }
 
-    var shape = r.polygon(lineEndx, LineEndy, raindropGroup);
     var spacing = 360 / points;
 
     for (var i = 0; i < randomRingAmt; i++) {
-        for (var j = 0; j < 360; j++) {
+
+        var shape = r.polygon(lineEndx, LineEndy, raindropGroup);
+
+        for (var j = 0; j < points; j++) {
             ringTime = true;
-            var x = Math.cos(Rune.radians(j * spacing)) * i*radius;
-            var y = .5*Math.sin(Rune.radians(j * spacing)) * i*radius;
+            var randomRadius = radius + Rune.random(2);
+            var x = Math.cos(Rune.radians(j * spacing)) * i * randomRadius;
+            //make Y a bit skinnier to create perspective
+            var y = .3 * Math.sin(Rune.radians(j * spacing)) * i * randomRadius;
+
             shape.lineTo(x, y).strokeDash("10,90," + dash1 / 2 + "," + dash2 / 2);
         }
     }
 
     var ringTime = false;
 
+    //make a dotted line
     var diagnalLine = r.line(lineStartx, lineSarty, lineEndx, LineEndy, raindropGroup).strokeDash(dash1 + "," + dash2);
-    //curveTo(control,vontrol,control,control,x,y)
+    //curveTo(control,control,control,control,x,y)
     var splash = r.path(lineEndx, LineEndy, raindropGroup)
-        // .curveTo(-100, -100, 0, 0)
         .curveTo(-splashWidth / 2, -100, -splashWidth, -30);
 
-    var puddle = r.polygon(lineEndx, LineEndy, raindropGroup);
-    puddle.lineTo
-    var shape = r.polygon(r.width / 2, 125 + (i * 200));
+    //var puddle = r.polygon(lineEndx, LineEndy, raindropGroup);
+    //puddle.lineTo
+    //r.polygon(r.width / 2, 125 + (i * 200));
 
     for (var i = 0; i < raindropGroup.children.length; i++) {
         raindropGroup.children[i].strokeWidth(lineStroke).stroke(white).strokeCap("round").fill(false);
@@ -103,9 +196,6 @@ function Raindrop(lineStartx, lineSarty, lineEndx, LineEndy, lineStroke, dash1, 
 
 
 
-
-
-
-
-//ignore, just make sure it's there
 r.draw();
+
+}

947cc5857d398e85e9a3f182870422bedaa402a5
Eve Weinberg <eve@neveroddoreven.tv>
2016-04-17 23:20:58 -0400

    morrree

--- FINAL/WK1/ImgtracerTest1.js
+++ FINAL/WK1/ImgtracerTest1.js
@@ -1,5 +1,5 @@
 var path = '../images/Faces_0.png'
-// ImageTracer.imageToSVG( path, alert );
+    // ImageTracer.imageToSVG( path, alert );
 
 
 // Almost the same with options, and the ImageTracer.appendSVGString callback will append the SVG
@@ -24,16 +24,32 @@ var path = '../images/Faces_0.png'
 // 
 ImageTracer.loadImage(
     path,
-    function(canvas){
+    function(canvas) {
 
         // Getting ImageData from canvas with the helper function getImgdata().
-        var imgd = ImageTracer.getImgdata( canvas );
+        var imgd = ImageTracer.getImgdata(canvas);
 
         // Synchronous tracing to SVG string
-        var svgstr = ImageTracer.imagedataToSVG( imgd, { pathomit:152,lcpr:0,scale:1, numberofcolors:12, blurradius:10 } );
+        var svgstr = ImageTracer.imagedataToSVG(imgd, { pathomit: 152, lcpr: 0, scale: 1, numberofcolors: 12, blurradius: 10 });
 
         // Appending SVG
-        ImageTracer.appendSVGString( svgstr, 'svgcontainer' );
+        ImageTracer.appendSVGString(svgstr, 'svgcontainer');
+        // svgstr.length is 263794
+        console.log(svgstr)
+        var commands = svgstr.split(/(?=[LMCQ])/);
+        console.log(commands)
+
+        var pointArrays = commands.map(function(d) {
+            var pointsArray = d.slice(1, d.length).split(',');
+            var pairsArray = [];
+            for (var i = 0; i < pointsArray.length; i += 2) {
+                pairsArray.push([+pointsArray[i], +pointsArray[i + 1]]);
+            }
+            return pairsArray;
+        });
+        console.log(pointArrays)
+        // var newdata = svgstr.node();
+        // console.log(newdata)
 
     }
 );

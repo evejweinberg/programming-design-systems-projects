d1aaeec399fcc9040ea21ce280faa635d1688c9f
Eve Weinberg <eve@neveroddoreven.tv>
2016-04-25 10:11:07 -0400

    rune

--- FINAL/WK1/ImgtracerTest1.js
+++ FINAL/WK1/ImgtracerTest1.js
@@ -19,11 +19,11 @@ ImageTracer.loadImage(
         // Appending SVG
         // ImageTracer.appendSVGString(svgstr, 'svgcontainer');
 
-        console.log(String(svgstr))
-        return svgToRune(svgstr)
+        return svgToRune(String(svgstr))
+            // console.log(svgToRune(String(svgstr)))
 
         // svgstr.length is 263794
-        
+
         // var commands = svgstr.split(/(?=[LMCQ])/);
         // console.log(commands)
 
@@ -48,35 +48,36 @@ ImageTracer.loadImage(
 
 
 function svgToRune(svg) {
+    // console.log(svg)
+    // make jquery element from the svg element
+    var jsvg = $(svg);
 
-  // make jquery element from the svg element
-  var jsvg = $(svg);
+    // loop through each path in the svg
+    jsvg.find('path').each(function() {
 
-  // loop through each path in the svg
-  jsvg.find('path').each(function() {
+        // convert path to jquery element and get the fill
+        // and stroke from the element
+        var path = $(this);
+        var rgbFill = path.attr('fill').replace('rgb(', '').replace('rgba(', '').replace(')', '').split(',');
+        var rgbStroke = path.attr('stroke').replace('rgb(', '').replace('rgba(', '').replace(')', '').split(',');
+        // console.log(rgbFill[0])
+            // create new rune.js path
+        var rpath = r.path(0, 0)
+            .fill(parseInt(rgbFill[0]), parseInt(rgbFill[1]), parseInt(rgbFill[2]))
+            .stroke(parseInt(rgbStroke[0]), parseInt(rgbStroke[1]), parseInt(rgbStroke[2]))
 
-    // convert path to jquery element and get the fill
-    // and stroke from the element
-    var path = $(this);
-    var rgbFill = path.attr('fill').replace('rgb(', '').replace('rgba(', '').replace(')', '').split(',');
-    var rgbStroke = path.attr('stroke').replace('rgb(', '').replace('rgba(', '').replace(')', '').split(',');
+        // loop through all path data and call corresponding Rune.js functions.
+        var d = path.attr('d');
+        var cmds = parsePath(d);
+        console.log(cmds)
+        for (var i = 0; i < cmds.length; i++) {
+            var cmd = cmds[i];
+            if (cmd[0] == 'M') rpath.moveTo(cmd[1], cmd[2]);
+            else if (cmd[0] == 'L') rpath.lineTo(cmd[1], cmd[2]);
+            else if (cmd[0] == 'Q') rpath.curveTo(cmd[1], cmd[2], cmd[3], cmd[4]);
+            else if (cmd[0] == 'Z') rpath.closePath();
+        }
+    });
 
-    // create new rune.js path
-    var rpath = r.path(0, 0)
-      .fill(parseInt(rgbFill[0]), parseInt(rgbFill[1]), parseInt(rgbFill[2]))
-      .stroke(parseInt(rgbStroke[0]), parseInt(rgbStroke[1]), parseInt(rgbStroke[2]))
-
-    // loop through all path data and call corresponding Rune.js functions.
-    var d = path.attr('d');
-    var cmds = parsePath(d);
-    for(var i = 0; i < cmds.length; i++) {
-      var cmd = cmds[i];
-      if(cmd[0] == 'M')      rpath.moveTo(cmd[1], cmd[2]);
-      else if(cmd[0] == 'L') rpath.lineTo(cmd[1], cmd[2]);
-      else if(cmd[0] == 'Q') rpath.curveTo(cmd[1], cmd[2], cmd[3], cmd[4]);
-      else if(cmd[0] == 'Z') rpath.closePath();
-    }
-  });
-
-  r.draw();
+    r.draw();
 }
